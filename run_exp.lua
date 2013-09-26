9f2a747f459f85dca265852b829f8d65e29e2b08
Michael 'myrhev' Mathieu <mathieu@cs.nyu.edu>
2013-09-26 14:56:02 -0400

    Sampling on the grid

--- run_exp.lua
+++ run_exp.lua
@@ -1,6 +1,33 @@
 require 'optim'
 require 'nn'
 
+function loopOnGrid(nValPerDim, v, i)
+   if v[i] < nValPerDim then
+      v[i] = v[i] + 1
+      if i < v:size(1) then
+	 return i+1
+      else
+	 return i
+      end
+   else
+      v[i] = 0
+      if i > 0 then
+	 return loopOnGrid(nValPerDim, v, i+1)
+      else
+	 return nil
+      end
+   end	 
+end
+
+--debug
+v = torch.zeros(3)
+i = 1
+print(v)
+while i ~= nil do
+   loopOnGrid(2, v, i)
+   print (v)
+end
+
 function analyze_model(model, criterion, grid, data) 
 	
 	local param,gradParam=model:getParameters()
