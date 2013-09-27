d8d2317fde9ea61ae7ba1615846b8362793d25b6
Mikael <mbh305@nyu.edu>
2013-09-27 17:12:33 -0400

    Added Hessian computation

--- run_exp.lua
+++ run_exp.lua
@@ -1,15 +1,15 @@
 require 'optim'
 require 'nn'
 require 'image'
-
-
+require 'finite_diff'
 
 printf = function(s,...)
 	return io.write(s:format(...))
 end
 
 
-function analyze_model(model, criterion, nPtsPerDim, gridRange, data) 
+function analyze_model(model, criterion, nPtsPerDim, gridRange, data, gradThreshold) 
+	local criticalPts={}
 	local grid = torch.linspace(-gridRange/2,gridRange/2,nPtsPerDim)
 	local param,gradParam=model:getParameters()
 	local D=param:size(1)
@@ -24,22 +24,35 @@ function analyze_model(model, criterion, nPtsPerDim, gridRange, data)
                 	param[j] = grid[ indx[j] ]
 		end
 		gradParam:zero()
-
-		local loss=0
-		for k=1,data[1]:size(1) do
-			local input=data[1][k]
-			local target=data[2][k]
-			local output=model:forward(input)
-			loss=loss+criterion:forward(output,target)
-			local dL_do=criterion:backward(output,target)
-			model:backward(input,dL_do)
+		
+		local function feval(x)
+			if x ~= param then
+				param:copy(x)
+			end
+			local loss=0
+			for k=1,data[1]:size(1) do
+				local input=data[1][k]
+				local target=data[2][k]
+				local output=model:forward(input)
+				loss=loss+criterion:forward(output,target)
+				local dL_do=criterion:backward(output,target)
+				model:backward(input,dL_do)
+			end
+			loss = loss/data[1]:size(1)
+			gradParam:div(data[1]:size(1))
+			return loss, gradParam
 		end
-		loss = loss/data[1]:size(1)
-		gradParam:div(data[1]:size(1))
+		loss,_ = feval(param)
 		lossValues[indx:storage()]=loss
-		gradValues[indx:storage()]=gradParam:norm()
+		local gradNorm=gradParam:norm()
+		gradValues[indx:storage()]=gradNorm
+		if gradNorm < gradThreshold then
+			hessian=hessianPerturbationNet(feval, param, 1e-3)
+			e=torch.eig(hessian)
+			criticalPts[#criticalPts+1]={eigenvals=e,param=param:clone(),indx=indx:clone()}
+		end
 	end
-	return lossValues,gradValues
+	return lossValues,gradValues,criticalPts
 end
 
 nSamples=100
@@ -51,7 +64,7 @@ criterion=nn.MSECriterion()
 data=torch.randn(nSamples):repeatTensor(2,1)
 data=data:reshape(data:size(1),data:size(2),1)
 
-loss,gradNorm=analyze_model(model,criterion,50,4,data)
+loss,gradNorm,criticalPts=analyze_model(model,criterion,50,4,data,0.3)
 gradNorm:reshape(gradNorm:nElement())
 image.display(loss)
 gnuplot.hist(gradNorm,100)
