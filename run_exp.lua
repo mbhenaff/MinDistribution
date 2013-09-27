bf9012c91f657ee2329f03340271ad1480f456f6
Mikael <mbh305@nyu.edu>
2013-09-27 16:29:24 -0400

    Added code to calculate loss function on grid, compute gradient norm on grid and plot

--- run_exp.lua
+++ run_exp.lua
@@ -1,43 +1,58 @@
 require 'optim'
 require 'nn'
+require 'image'
 
-function loopOnGrid(nValPerDim, v, i)
-   if v[i] < nValPerDim then
-      v[i] = v[i] + 1
-      if i < v:size(1) then
-	 return i+1
-      else
-	 return i
-      end
-   else
-      v[i] = 0
-      if i > 0 then
-	 return loopOnGrid(nValPerDim, v, i+1)
-      else
-	 return nil
-      end
-   end	 
+
+printf = function(s,...)
+	return io.write(s:format(...))
 end
 
---debug
-v = torch.zeros(3)
-i = 1
-print(v)
-while i ~= nil do
-   loopOnGrid(2, v, i)
-   print (v)
-end
-
-function analyze_model(model, criterion, grid, data) 
-	
+function analyze_model(model, criterion, nPtsPerDim, gridRange, data) 
+	local grid = torch.linspace(-gridRange/2,gridRange/2,nPtsPerDim)
 	local param,gradParam=model:getParameters()
-	gradParam:zero()
-	local output=model:forward(data[1])
-	local loss=criterion:forward(output,data[2])
-	local dL_do=criterion:backward(output,data[2])
-	model:backward(data[1],dL_do)
-	
-	
+	local D=param:size(1)
+	local lossValues=torch.Tensor((torch.ones(D)*nPtsPerDim):long():storage())
+	local gradValues=torch.Tensor((torch.ones(D)*nPtsPerDim):long():storage())
+	local indx = torch.LongTensor(D)
 
+	for i=0,nPtsPerDim^D-1 do
+		xlua.progress(i,nPtsPerDim^D-1)
+        	for j=1,D do
+			indx[j] = math.mod(math.floor(i/nPtsPerDim^(j-1)),nPtsPerDim)+1
+                	param[j] = grid[ indx[j] ]
+		end
+		gradParam:zero()
 
+		local loss=0
+		for k=1,data[1]:size(1) do
+			local input=data[1][k]
+			local target=data[2][k]
+			local output=model:forward(input)
+			loss=loss+criterion:forward(output,target)
+			local dL_do=criterion:backward(output,target)
+			model:backward(input,dL_do)
+		end
+		loss = loss/data[1]:size(1)
+		gradParam:div(data[1]:size(1))
+		lossValues[indx:storage()]=loss
+		gradValues[indx:storage()]=gradParam:norm()
+	end
+	return lossValues,gradValues
 end
+
+nSamples=100
+dimInputs=1
+model = nn.Sequential()
+model:add(nn.Mul(dimInputs))
+model:add(nn.Mul(dimInputs))
+criterion=nn.MSECriterion()
+data=torch.randn(nSamples):repeatTensor(2,1)
+data=data:reshape(data:size(1),data:size(2),1)
+
+loss,gradNorm=analyze_model(model,criterion,50,4,data)
+gradNorm:reshape(gradNorm:nElement())
+image.display(loss)
+gnuplot.hist(gradNorm,100)
+
+
+
